4c3cf4a72004a6da4e16aa51ef7dd42f40cf605e
Manish Patel <maneeshkurmii@gmail.com>
2025-10-18 18:27:45 +0530

    feat: Updated footer and index.html with new enhacement

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white text-center py-4">
-      © 2025 mindscribe. All rights reserved.
+      © 2025 Reflection. All rights reserved.
     </footer>
   );
 };
